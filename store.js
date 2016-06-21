97a3b8fe00ec388f6bd013e1eca883760c96d5ec
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-06-21 15:09:21 +0530

    added some more records

--- store.js
+++ store.js
@@ -5,6 +5,14 @@ const initialState = [
     {
         id: 'S1',
         summary: 'test issue 1'
+    },
+    {
+        id: 'S2',
+        summary: 'test issue 2'
+    },
+    {
+        id: 'S3',
+        summary: 'test issue 3'
     }
 ]
 
